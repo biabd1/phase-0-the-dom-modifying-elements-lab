69e39c8e40f2b82f9c06902c96bc479ef69aa00a
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-12 04:23:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+ main.remove()
+ const newHeader= document.createElement('h1')
+ newHeader.id = "victory"
+ newHeader.innerHTML= ("<h1>Bilal is the champion</h1>")
+ document.body.append(newHeader)
